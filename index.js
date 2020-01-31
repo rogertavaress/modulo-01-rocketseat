733725882b79149260512597844677ae40466561
Rogério Tavares <rogerioctf@gmail.com>
2020-01-30 22:41:36 -0300

    Aula 02

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  return res.json({ message: "Olá Mundo!" });
+// http://localhost:3000/teste/1
+server.get("/teste/:id", (req, res) => {
+  const { id } = req.params;
+
+  return res.json({ message: `Olá Mundo de ${id}!` });
+});
+
+// http://localhost:3000/teste?id=1
+server.get("/teste", (req, res) => {
+  const { id } = req.query;
+
+  return res.json({ message: `Olá Mundo de ${id}!` });
 });
 
 server.listen(3000);
