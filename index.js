c91242039102d2927409ef7a6b7dadc5e7acd541
Rogério Tavares <rogerioctf@gmail.com>
2020-01-30 23:58:26 -0300

    Aula 06

--- index.js
+++ index.js
@@ -6,17 +6,41 @@ server.use(express.json());
 
 const users = ["Roger", "Suelen", "Violeta", "Matilda", "Lavínia"];
 
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log("A Requisição foi chamada!");
+  console.log(`Método: ${req.method}`);
+  console.log(`URL: ${req.url}`);
+  next();
+  console.log("Finalizou!");
+  console.timeEnd("Request");
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.nome) {
+    return res.status(400).json({ message: "User name is required" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  if (!users[req.params.id]) {
+    return res.status(400).json({ message: "User does not exists" });
+  }
+}
+
 server.get("/users", (req, res) => {
   return res.json({ Users: users });
 });
 
-server.get("/users/:id", (req, res) => {
+server.get("/users/:id", checkUserInArray, (req, res) => {
   const { id } = req.params;
 
   return res.json({ Nome: users[id] });
 });
 
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { nome } = req.body;
 
   users.push(nome);
@@ -24,7 +48,7 @@ server.post("/users", (req, res) => {
   return res.json({ Users: users });
 });
 
-server.put("/users/:id", (req, res) => {
+server.put("/users/:id", checkUserExists, checkUserInArray, (req, res) => {
   const { id } = req.params;
   const { nome } = req.body;
 
@@ -33,7 +57,7 @@ server.put("/users/:id", (req, res) => {
   return res.json({ Users: users });
 });
 
-server.delete("/users/:id", (req, res) => {
+server.delete("/users/:id", checkUserInArray, (req, res) => {
   const { id } = req.params;
 
   users.splice(id, 1);
